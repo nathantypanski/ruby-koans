80801a4303d843cd78f3438b510570dc0646f85a
Nathan Typanski <ntypanski@gmail.com>
2015-05-17 18:08:13 -0400

    complete about_triangle_project_2

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  if !(a + b > c && a + c > b && b + c > a) then
+    raise TriangleError
+  end
   if a == b && b == c then
       :equilateral
   elsif a == b || a == c || b == c then
